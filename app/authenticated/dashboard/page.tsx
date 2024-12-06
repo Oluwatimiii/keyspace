467c1bf0923884f2484147d27acdf5fdcadeda55
Oke Oluwatimilehin <oketimilehin32@gmail.com>
2024-12-06 01:13:56 +0100

    fix: deployment

--- app/authenticated/dashboard/page.tsx
+++ app/authenticated/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { LogoutButton } from "@/components/Auth/LogoutButton"
 import { Button } from "@/components/ui/button"
 
 export default function DashboardPage() {
-
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Your Dashboard</h1>
